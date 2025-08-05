b4f8be3491acc566723bbd17ae1af795a93316e1
Lester Hurtado <lester@lyflyn.net>
2025-08-06 07:54:32 +0800

    add note, recipe and cheat sheet in subjects

--- app/Enums/Subject.php
+++ app/Enums/Subject.php
@@ -4,15 +4,17 @@ namespace App\Enums;
 
 enum Subject: string
 {
-    const NOTE = 'note';
+    case NOTE = 'Note';
     case QUOTE = 'Quote';
     case POEM = 'Poem';
     case INSTRUCTIONS = 'Instructions';
     case SONG = 'Song';
-    case HAIKU = 'Haiku';
-    case BLOG = 'Blog';
+//    case HAIKU = 'Haiku';
+//    case BLOG = 'Blog';
     case TRIVIA = 'Trivia';
     case JOKE = 'Joke';
+    case RECIPE = 'Recipe';
+    case CHEAT_SHEET = 'Cheat Sheet';
 
     public static function options(): array
     {
@@ -38,10 +40,12 @@ enum Subject: string
             self::POEM => 'e.g. The Road Not Taken by Robert Frost',
             self::INSTRUCTIONS => 'e.g. How to Brew Tea',
             self::SONG => 'e.g. Disenchanted - My Chemical Romance',
-            self::HAIKU => 'e.g. Old pond... a frog jumps in',
-            self::BLOG => 'e.g. 5 Lessons I Learned from Freelancing',
+//            self::HAIKU => 'e.g. Old pond... a frog jumps in',
+//            self::BLOG => 'e.g. 5 Lessons I Learned from Freelancing',
             self::TRIVIA => 'e.g. Chemistry',
             self::JOKE => 'e.g. Green Joke',
+            self::RECIPE => 'e.g. Caldereta',
+            self::CHEAT_SHEET => 'e.g. Cheat Sheet',
         };
     }
 
