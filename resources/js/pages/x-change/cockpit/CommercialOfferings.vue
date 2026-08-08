ec7444a795e79d33c338b05cc419f2fbd70a7696
Lester Hurtado <lester@lyflyn.net>
2026-08-08 14:21:22 +0800

    type commercial workspace read model

--- resources/js/pages/x-change/cockpit/CommercialOfferings.vue
+++ resources/js/pages/x-change/cockpit/CommercialOfferings.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
-import CommercialOfferings from '../../../cockpit/pages/CommercialOfferings.vue';
+import CommercialOfferings from "../../../cockpit/pages/CommercialOfferings.vue";
+
+type CommercialOfferingsProps = InstanceType<
+  typeof CommercialOfferings
+>["$props"];
 
 defineProps<{
-    cockpit_header_read_model?: Record<string, unknown>;
-    commercial_offering: Record<string, unknown>;
+  cockpit_header_read_model?: Record<string, unknown>;
+  commercial_offering: CommercialOfferingsProps["commercialOffering"];
 }>();
 </script>
 
 <template>
-    <CommercialOfferings
-        :cockpit-header-read-model="cockpit_header_read_model"
-        :commercial-offering="commercial_offering"
-    />
+  <CommercialOfferings
+    :cockpit-header-read-model="cockpit_header_read_model"
+    :commercial-offering="commercial_offering"
+  />
 </template>
